8874a7205a51cc34fcf5091f21b811c6885cbb7e
Corey Scheideman <corscheid@gmail.com>
2021-03-01 19:40:55 -0600

    Replace FCC testing js with ng e2e tests

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -2,22 +2,86 @@ import { browser, logging } from 'protractor';
 import { AppPage } from './app.po';
 
 describe('workspace-project App', () => {
-  let page: AppPage;
+  describe('Content', () => {
+    let page: AppPage;
 
-  beforeEach(() => {
-    page = new AppPage();
-  });
+    beforeEach(() => {
+      page = new AppPage();
+    });
 
-  it('should display welcome message', async () => {
-    await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('fcc-random-quote-machine-angular app is running!');
-  });
+    it('should display quote box', async () => {
+      await page.navigateTo();
+      expect(await page.getQuoteBox()).toBeTruthy();
+    });
 
-  afterEach(async () => {
-    // Assert that there are no errors emitted from the browser
-    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
-    expect(logs).not.toContain(jasmine.objectContaining({
-      level: logging.Level.SEVERE,
-    } as logging.Entry));
-  });
+    it('should display text element inside quote box with random quote', async () => {
+      expect(await page.getQuoteBoxText()).toBeTruthy();
+    });
+
+    it(`should display author element inside quote box with quote's author`, async () => {
+      expect(await page.getQuoteBoxAuthor()).toBeTruthy();
+    })
+
+    it('should display "New quote" button inside quote box', async () => {
+      expect(await page.getNewQuoteButtonText()).toEqual('New quote');
+    });
+
+    it('should display "Tweet" button inside quote box', async () => {
+      expect(await page.getTweetButtonText()).toEqual('Tweet');
+    });
+
+    it('should fetch new quote when "New quote" button is clicked', async () => {
+      const initialQuoteText = await page.getQuoteBoxText();
+      await page.clickQuoteButton();
+      const newQuoteText = await page.getQuoteBoxText();
+      expect(initialQuoteText).toBeTruthy();
+      expect(newQuoteText).toBeTruthy();
+      expect(newQuoteText).not.toEqual(initialQuoteText);
+    });
+
+    it(`should update new quote's author when "New quote" button is clicked`, async () => {
+      const initialAuthor = await page.getQuoteBoxAuthor();
+      await page.clickQuoteButton();
+      const newAuthor = await page.getQuoteBoxAuthor();
+      expect(initialAuthor).toBeTruthy();
+      expect(newAuthor).toBeTruthy();
+      expect(newAuthor).not.toEqual(initialAuthor);
+    });
+
+    it('should open Twitter tweet intent when "Tweet" button is clicked', async () => {
+      expect(await page.getTweetURL()).toMatch(/^https:\/\/twitter\.com\/intent\/tweet/);
+    });
+
+    afterEach(async () => {
+      // Assert that there are no errors emitted from the browser
+      const logs = await browser.manage().logs().get(logging.Type.BROWSER);
+      expect(logs).not.toContain(jasmine.objectContaining({
+        level: logging.Level.SEVERE,
+      } as logging.Entry));
+    });
+  })
+
+  describe('Layout', () => {
+    let page: AppPage;
+
+    beforeEach(() => {
+      page = new AppPage();
+    });
+
+    it('should display the quote box in the center horizontally', async () => {
+      const htmlElementBounds = await page.getHtmlElementBounds();
+      const quoteBoxBounds = await page.getQuoteBoxBounds();
+      const left = quoteBoxBounds.x0 - htmlElementBounds.x0;
+      const right = htmlElementBounds.x1 - quoteBoxBounds.x1;
+      expect(left).toEqual(right);
+    });
+
+    afterEach(async () => {
+      // Assert that there are no errors emitted from the browser
+      const logs = await browser.manage().logs().get(logging.Type.BROWSER);
+      expect(logs).not.toContain(jasmine.objectContaining({
+        level: logging.Level.SEVERE,
+      } as logging.Entry));
+    });
+  })
 });
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,75 @@
-import { browser, by, element } from 'protractor';
+import { browser, by, element, ElementFinder } from 'protractor';
 
 export class AppPage {
   async navigateTo(): Promise<unknown> {
     return browser.get(browser.baseUrl);
   }
 
-  async getTitleText(): Promise<string> {
-    return element(by.css('app-root .content span')).getText();
+  async getQuoteBox(): Promise<string> {
+    let quoteBox: ElementFinder = element(by.css('app-root app-quote-box #quote-box'));
+    let quoteBoxContent: string = await quoteBox.getText();
+    return quoteBoxContent;
+  }
+
+  async getQuoteBoxText(): Promise<string> {
+    let quoteBoxText: ElementFinder = element(by.css('app-root app-quote-box #quote-box #text'));
+    let quoteBoxTextContent: string = await quoteBoxText.getText();
+    return quoteBoxTextContent;
+  }
+
+  async getQuoteBoxAuthor(): Promise<string> {
+    let quoteBoxAuthor: ElementFinder = element(by.css('app-root app-quote-box #quote-box #author'));
+    let quoteBoxAuthorContent: string = await quoteBoxAuthor.getText();
+    return quoteBoxAuthorContent;
+  }
+
+  async getNewQuoteButtonText(): Promise<string> {
+    let newQuoteButton: ElementFinder = element(by.css('app-root app-quote-box #quote-box .btn-row #new-quote'));
+    let newQuoteButtonText: string = await newQuoteButton.getText();
+    return newQuoteButtonText;
+  }
+
+  async getTweetButtonText(): Promise<string> {
+    let tweetButton: ElementFinder = element(by.css('app-root app-quote-box #quote-box .btn-row #tweet-quote'));
+    let tweetButtonText: string = await tweetButton.getText();
+    return tweetButtonText;
+  }
+
+  async clickQuoteButton(): Promise<void> {
+    let newQuoteButton: ElementFinder = element(by.css('app-root app-quote-box #quote-box .btn-row #new-quote'));
+    await newQuoteButton.click();
+  }
+
+  async clickTweetButton(): Promise<void> {
+    let tweetButton: ElementFinder = element(by.css('app-root app-quote-box #quote-box .btn-row #tweet-quote'));
+    await tweetButton.click();
+  }
+
+  async getTweetURL(): Promise<string> {
+    let tweetButton: ElementFinder = element(by.css('app-root app-quote-box #quote-box .btn-row #tweet-quote'));
+    let tweetButtonURL = await tweetButton.getAttribute('href');
+    return tweetButtonURL;
+  }
+
+  async getHtmlElementBounds() {
+    let htmlElement: ElementFinder = element(by.tagName('html'));
+    let htmlElementSize: { width: number, height: number } = await htmlElement.getSize();
+    let htmlElementLocation: { x: number, y: number } = await htmlElement.getLocation();
+    let htmlElementBounds: { x0: number, x1: number } = {
+      x0: htmlElementLocation.x,
+      x1: htmlElementLocation.x + htmlElementSize.width
+    };
+    return htmlElementBounds;
+  }
+
+  async getQuoteBoxBounds() {
+    let quoteBox: ElementFinder = element(by.css('app-root app-quote-box #quote-box'));
+    let quoteBoxSize: { width: number, height: number } = await quoteBox.getSize();
+    let quoteBoxLocation: { x: number, y: number } = await quoteBox.getLocation();
+    let quoteBoxBounds: { x0: number, x1: number } = {
+      x0: quoteBoxLocation.x,
+      x1: quoteBoxLocation.x + quoteBoxSize.width,
+    };
+    return quoteBoxBounds;
   }
 }
